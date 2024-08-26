66e19cab9aff7a19ea707101be54f7b79ab08ac0
Kareem Mohamed <63722@fa-hiss.edu.eg>
2024-08-26 10:48:33 +0300

    location tracker

---
+++ app/src/main/java/com/example/weatherapp/data/location/DefaultLocationTracker.kt
@@ -0,0 +1,73 @@
+package com.example.weatherapp.data.location
+
+import com.example.weatherapp.domain.location.LocationTracker
+
+import android.Manifest
+import android.app.Application
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.os.Looper
+import androidx.core.content.ContextCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import kotlinx.coroutines.suspendCancellableCoroutine
+import javax.inject.Inject
+import kotlin.coroutines.resume
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.Priority
+
+
+class DefaultLocationTracker @Inject constructor(
+    private val locationClient: FusedLocationProviderClient,
+    private val application: Application
+) : LocationTracker {
+
+    override suspend fun getCurrentLocation(): Location? {
+        val hasAccessFineLocationPermission = ContextCompat.checkSelfPermission(
+            application,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+        val hasAccessCoarseLocationPermission = ContextCompat.checkSelfPermission(
+            application,
+            Manifest.permission.ACCESS_COARSE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
+        val locationManager =
+            application.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if (!hasAccessCoarseLocationPermission || !hasAccessFineLocationPermission || !isGpsEnabled) {
+            return null
+        }
+
+        return suspendCancellableCoroutine { cont ->
+            val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000L)
+                .setMinUpdateIntervalMillis(2000L)
+                .setMinUpdateIntervalMillis(1000L)
+                .build()
+
+            val locationCallback = object : LocationCallback() {
+                override fun onLocationResult(result: LocationResult) {
+                    super.onLocationResult(result)
+                    result.locations.lastOrNull()?.let { location ->
+                        cont.resume(location)
+                        locationClient.removeLocationUpdates(this)
+                    }
+                }
+            }
+
+            locationClient.requestLocationUpdates(
+                locationRequest,
+                locationCallback,
+                Looper.getMainLooper()
+            )
+
+            cont.invokeOnCancellation {
+                locationClient.removeLocationUpdates(locationCallback)
+            }
+        }
+    }
+}
